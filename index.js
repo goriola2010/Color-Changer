c9808bac53af4923b9ae9352eb9df506653bace4
goriola2010 <goriolaagbaje@gmail.com>
2025-04-16 00:26:40 +0100

    Second Commit

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 alert('Welcome to Itadori Page')
 function color(value) {
     document.body.style.backgroundColor = value
-}
+};
+const toggleButton = document.getElementById('toggleButton');
+const myParagraph = document.getElementById('myParagraph');
+
+ function toggler() {
+    if(myParagraph.style.display === 'none') {
+        myParagraph.style.display = 'block'
+    } else {
+        myParagraph.style.display = 'none'
+    }
+};
